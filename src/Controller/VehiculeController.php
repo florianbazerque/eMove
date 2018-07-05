e81d54f6e0e9314f8c99f573bad1364ed5c6f471
Florian Emmanuel <Flo>
2018-07-05 11:09:10 +0200

    eMove v.0.3 Ajout Shop + Modif CSS

---
+++ src/Controller/VehiculeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silve
+ * Date: 05/07/2018
+ * Time: 10:56
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class VehiculeController extends AbstractController
+{
+    /**
+     *  @Route("/shop", name="shop")
+     */
+    public function vehiculeAction()
+    {
+        return $this->render('default/shop.html.twig');
+    }
+}
