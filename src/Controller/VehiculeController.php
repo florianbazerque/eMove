b7b3e06be4c22f5daedc8d88182ab978788305e4
Florian Emmanuel <Flo>
2018-07-10 23:11:31 +0200

    eMove Vehicule Front

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -8,25 +8,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Vehicule;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VehiculeController extends AbstractController
 {
+
     /**
      *  @Route("/shop", name="shop")
      */
     public function listAction()
     {
-        return $this->render('default/shop.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $vehicules = $em->getRepository(Vehicule:: class)
+            ->findAll();
+        return $this->render('default/shop.html.twig', [
+            'vehicules' => $vehicules
+        ]);
     }
 
     /**
-     *  @Route("/produit", name="produit")
+     *  @Route("/produit/{id}", name="produit_view", requirements={"id"="\d+"})
      */
-    public function vehiculeAction()
+    public function viewAction($id)
     {
-        return $this->render('default/produit.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $vehicule = $em->getRepository(Vehicule:: class)
+            ->find($id);
+        if (!$vehicule) {
+            throw $this->createNotFoundException(
+                'No vehicule found for id '.$id
+            );
+        }elseif ($vehicule == null){
+            throw new HttpException(400, "New comment is not valid.");
+        } else {
+            return $this->render('default/produit.html.twig', [
+                'vehicule' => $vehicule
+            ]);
+        }
     }
+
+    /**
+     *  @Route("/produit", name="produit_error")
+     */
+    public function errorAction()
+    {
+        throw new HttpException(400, "Aucune voiture selectionner");
+    }
+
 }
